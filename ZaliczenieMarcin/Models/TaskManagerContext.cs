0cce73ee97fb0970e2ea94b89c9c0302d28e43c2
emptaj <cinkowski07@gmail.com>
2021-06-26 12:15:39 +0200

    init

---
+++ ZaliczenieMarcin/Models/TaskManagerContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZaliczenieMarcin.Models
+{
+    public class TaskManagerContext : DbContext
+    {
+        
+        public TaskManagerContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<TaskModel> Tasks { get; set; }
+
+
+    }
+}
---
+++ ZaliczenieMarcin/Repositories/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZaliczenieMarcin.Models;
+
+namespace ZaliczenieMarcin.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int id);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+    }
+}
